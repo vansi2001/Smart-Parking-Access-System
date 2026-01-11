0ba3bd001e89866b98f432e32a441603c323aa07
台鋼科大_金小士 <7q13124101@mail.tsust.edu.tw>
2026-01-12 00:13:36 +0800

    Fix: Remove venv from git and add gitignore

--- backend/database.py
+++ backend/database.py
@@ -2,9 +2,12 @@ from sqlalchemy import create_engine, Column, Integer, String, DateTime, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
+import os
 
 # 1. Cấu hình SQLite (File database sẽ tên là parking.db)
-SQLALCHEMY_DATABASE_URL = "sqlite:///./parking.db"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "parking.db")
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
